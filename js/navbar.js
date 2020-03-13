6a9b9301f03129a431129137a089a6385e749c4b
Jakub Zegar <jakubzegar3@gmail.com>
2020-03-13 18:53:12 +0100

    JS-based navigation bar added

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+let nav = '<nav><div class="nav-item"><a class="nav-link" href="index.html">Home</a></div>' +
+           '<div class="nav-item"><a class="nav-link" href="quiz.html">Quiz</a></div>' +
+            '<div class="nav-item"><a class="nav-link" href="contact.html">Kontakt</a></div></nav>';
+
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
---
+++ js/test.js
@@ -0,0 +1,10 @@
+for(let i = 1; i<11; ++i){
+    let line = ""
+    for(let j = 1; j<11; ++j) {
+
+        line += i*j + " ";
+    }
+    console.log(line)
+
+}
+
